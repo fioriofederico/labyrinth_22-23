0a44fd6dd2b0c1d3a16fabf8a80356dec3d293ef
Federico Fiorio <federico.fiorio@outlook.it>
2023-01-08 10:37:21 +0100

    Create a possibile fund path but now just work with matrix 0 and 1

---
+++ utilities/foundPath.py
@@ -0,0 +1,77 @@
+def maze2graph(maze):
+    height = len(maze)
+    width = len(maze[0]) if height else 0
+    graph = {(i, j): [] for j in range(width) for i in range(height) if not maze[i][j]}
+    for row, col in graph.keys():
+        if row < height - 1 and not maze[row + 1][col]:
+            graph[(row, col)].append(("S ", (row + 1, col)))
+            graph[(row + 1, col)].append(("N ", (row, col)))
+        if col < width - 1 and not maze[row][col + 1]:
+            graph[(row, col)].append(("E ", (row, col + 1)))
+            graph[(row, col + 1)].append(("W ", (row, col)))
+    return graph
+
+from heapq import heappop, heappush
+from collections import deque
+
+
+def find_path_dfs(maze):
+    #start, goal = (1, 0), (len(maze) - 2, len(maze[0]) - 2)
+    start, goal = (0, 0), (11, 1)
+    stack = deque([("", start)])
+    visited = set()
+    graph = maze2graph(maze)
+    while stack:
+        path, current = stack.pop()
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            stack.append((path + direction, neighbour))
+    return "NO WAY!"
+
+
+def heuristic(cell, goal):
+    return abs(cell[0] - goal[0]) + abs(cell[1] - goal[1])
+
+
+def find_path_astar(maze):
+    start, goal = (0, 0), (14, 8)
+    pr_queue = []
+    heappush(pr_queue, (0 + heuristic(start, goal), 0, "", start))
+    visited = set()
+    graph = maze2graph(maze)
+    while pr_queue:
+        _, cost, path, current = heappop(pr_queue)
+        if current == goal:
+            return path
+        if current in visited:
+            continue
+        visited.add(current)
+        for direction, neighbour in graph[current]:
+            heappush(pr_queue, (cost + heuristic(neighbour, goal), cost + 1,
+                                path + direction, neighbour))
+    return "NO WAY!"
+
+
+MAZE = [
+    [0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
+    [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 0, 0, 0, 1, 0, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1],
+]
+print(find_path_astar(MAZE))
