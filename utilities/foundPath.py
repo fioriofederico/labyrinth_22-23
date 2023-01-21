7d2c735dce57e5e239141ed47f3216181ebb7915
Federico Fiorio <federico.fiorio@outlook.it>
2023-01-21 10:38:40 +0100

    Aggiunto 2 punti di ritorno dati e aggiutno json

--- utilities/foundPath.py
+++ utilities/foundPath.py
@@ -1,12 +1,17 @@
 from heapq import heappop, heappush
 import numpy as np
+import json
 
 
 class FoundPath:
     # Nota fondamentale il primo valore rappresenta la riga il secondo la colonna essendo un array si conta sempre a partite da 0
+    __visited = {}
+    __path_return = []
+    __mydict = {}
     pass
 
     def __init__(self, maze, start, goal):
+        self.__path_returned_json = None
         self.__maze = maze
         self.__start = start
         self.__goal = goal
@@ -56,7 +61,7 @@ class FoundPath:
         Movement wih North East South West
         And Cost for moviment
     """
-
+    # Versione lasciata da rimuove
     def find_path_astar(self):
         start_input = self.__start
         goal = self.__goal
@@ -81,6 +86,7 @@ class FoundPath:
 
         return path_return
 
+    #Versione lasciata da rimuovere
     def find_multi_path_astar(self, num_paths = 3):
         goal = self.__goal
         path_return = []
@@ -103,4 +109,44 @@ class FoundPath:
             path_return.append("NO WAY!")
 
         return path_return
+
+    #Ultima opzione di codice
+    def find_multi_path_astar_return_visited(self, num_paths=3):
+        goal = self.__goal
+        for start in self.__start:
+            pr_queue = []
+            heappush(pr_queue, (0 + self.heuristic(start, goal), 0, "", start))
+            visited = [start]
+            graph: dict = self.maze2graph()
+            while len(self.__path_return) < num_paths * len(self.__start) and pr_queue:
+                _, cost, path, current = heappop(pr_queue)
+                if current == goal:
+                    self.__path_return.append((start, goal, path, cost))
+                    new_element = "percorso" + str(start)
+                    if new_element not in self.__mydict:
+                        self.__mydict[f"{new_element}"] = {}
+                    self.__mydict[f"{new_element}"]["start"] = start
+                    self.__mydict[f"{new_element}"]["goal"] = goal
+                    self.__mydict[f"{new_element}"]["cost"] = cost
+                    self.__mydict[f"{new_element}"]["path"] = path
+                    self.__mydict[f"{new_element}"]["pathVisited"] = visited
+                    self.__path_returned_json = json.dumps(self.__mydict)
+                    self.__visited[start] = visited
+                for direction, neighbour, real_cost in graph[current]:
+                    if neighbour not in visited:
+                        visited.append(neighbour)
+                        heappush(pr_queue, (cost + self.heuristic(neighbour, goal), cost + real_cost,
+                                            path + direction, neighbour))
+        if len(self.__path_return) < num_paths * len(self.__start):
+            self.__path_return.append("NO WAY!")
+        return self.__path_return
+
+    def getPathVisited(self):
+        return self.__visited
+
+    def getPathRetunrJson(self):
+        return self.__path_returned_json
+
+
+
 
