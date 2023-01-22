a961a211b5f3df8396f7300329181adf01e22803
Vincenzo Maria Calandra <vincenzomaria.calandra@gmail.com>
2023-01-22 16:47:49 +0100

    Merged

--- main.py
+++ main.py
@@ -6,10 +6,3 @@ from utilities.foundPath import FoundPath
 if __name__ == "__main__":
     p = Maze(20,30)
     p.generate()
-    p.printMaze()
-    p.getMazeJson()
-    p.generate()
-    p.getMazeImage()
-    p.readMazeJson("maze.json")
-    p.generate()
-    p.getMazeImage()
