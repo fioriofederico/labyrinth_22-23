1da3e68b73b629253c7d6eade6ca25ba1a7a08b7
Federico Fiorio <federico.fiorio@outlook.it>
2023-02-09 11:53:40 +0100

    Aggiunti commenti

--- utilities/newFoundPath.py
+++ utilities/newFoundPath.py
@@ -1,27 +1,49 @@
 from typing import List, Tuple
 from heapq import heappop, heappush
 
-
+#Definizione della funzione principale "dijkstra", che accetta come argomenti il labirinto,
+#la posizione di partenza (o più posizioni di partenza) e la posizione di destinazione.
 def deikstra(maze: List[List[int]], start: List[Tuple[int, int]], goal: Tuple[int, int]):
+    # Definizione delle variabili ROW e COL che
+    # rappresentano rispettivamente il numero di righe e colonne del labirinto.
     ROW, COL = len(maze), len(maze[0])
+    #Definizione della lista "directions" che rappresenta le direzioni
+    # possibili da esplorare nel labirinto (Est, Ovest, Nord, Sud).
     directions = ["E", "W", "N", "S"]
 
+
+    #Definizione della funzione "is_valid" che verifica se una posizione
+    # è valida (all'interno dei limiti del labirinto e non bloccata da un muro).
     def is_valid(x, y):
         return 0 <= x < ROW and 0 <= y < COL and maze[x][y] != 0
 
+    #Definizione della funzione "heuristic" che calcola la distanza
+    # euristica tra la posizione corrente e la posizione di destinazione
     def heuristic(current, goal):
         x1, y1 = current
         x2, y2 = goal
         return abs(x1 - x2) + abs(y1 - y2)
 
     def bfs(start):
+        # Inizializza la coda con la priorità, che in questo caso è la somma del costo attuale e dell'euristica dalla posizione corrente al goal
         pr_queue = []
         heappush(pr_queue, (0 + heuristic(start, goal), 0, "", "", start))
+
+        # Inizializza la lista dei nodi visitati
         visited = set()
+
+        # Inizializza la lista dei percorsi trovati
         paths = []
+
+        # Inizializza il dizionario che conterrà tutti i percorsi
         dictPath = {}
+
+        # Continua a ciclare finché non sono stati trovati 3 percorsi o la coda è vuota
         while len(paths) < 3 and pr_queue:
+            # Prendi il nodo con priorità minima dalla coda
             _, cost, path, path_cords, current = heappop(pr_queue)
+
+            # Se il nodo corrente è il goal, aggiungi il percorso alla lista dei percorsi e al dizionario
             if current == goal:
                 paths.append((start, goal, path, cost))
                 string = 'Opzione' + f'{len(paths)}'
@@ -33,9 +55,15 @@ def deikstra(maze: List[List[int]], start: List[Tuple[int, int]], goal: Tuple[in
                 dictPath[start][string]["goal"] = goal
                 dictPath[start][string]["path"] = path
                 dictPath[start][string]["cost"] = cost
+
+            # Se il nodo è già stato visitato, salta al prossimo
             if current in visited:
                 continue
+
+            # Aggiungi il nodo corrente alla lista dei nodi visitati
             visited.add(current)
+
+            # Espandi il nodo corrente in tutte le direzioni possibili
             x, y = current
             for direction in directions:
                 if direction == "E":
@@ -54,10 +82,11 @@ def deikstra(maze: List[List[int]], start: List[Tuple[int, int]], goal: Tuple[in
                     if is_valid(x + 1, y):
                         heappush(pr_queue, (cost + maze[x + 1][y] + heuristic((x + 1, y), goal), cost + maze[x + 1][y],
                                             path + direction, path_cords + str((x + 1, y)), (x + 1, y)))
+
+        #Viene ritornato o il percorso o No WAY
         return dictPath or ["NO WAY!"]
 
     paths = []
-    dict = {}
     for s in start:
         path = bfs(s)
         if not path == ['NO WAY!']:
