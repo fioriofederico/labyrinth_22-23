ce492087179534de499e2604bdd481b34ede75e9
Federico Fiorio <federico.fiorio@outlook.it>
2023-02-09 11:43:54 +0100

    modifiche per un nuovo found

--- utilities/newFoundPath.py
+++ utilities/newFoundPath.py
@@ -19,10 +19,20 @@ def deikstra(maze: List[List[int]], start: List[Tuple[int, int]], goal: Tuple[in
         heappush(pr_queue, (0 + heuristic(start, goal), 0, "", "", start))
         visited = set()
         paths = []
+        dictPath = {}
         while len(paths) < 3 and pr_queue:
             _, cost, path, path_cords, current = heappop(pr_queue)
             if current == goal:
                 paths.append((start, goal, path, cost))
+                string = 'Opzione' + f'{len(paths)}'
+                if start not in dictPath:
+                    dictPath[start] = {}
+                if string not in dictPath:
+                    dictPath[start][string] = {}
+                dictPath[start][string]["start"] = start
+                dictPath[start][string]["goal"] = goal
+                dictPath[start][string]["path"] = path
+                dictPath[start][string]["cost"] = cost
             if current in visited:
                 continue
             visited.add(current)
@@ -44,22 +54,36 @@ def deikstra(maze: List[List[int]], start: List[Tuple[int, int]], goal: Tuple[in
                     if is_valid(x + 1, y):
                         heappush(pr_queue, (cost + maze[x + 1][y] + heuristic((x + 1, y), goal), cost + maze[x + 1][y],
                                             path + direction, path_cords + str((x + 1, y)), (x + 1, y)))
-        return paths or ["NO WAY!"]
+        return dictPath or ["NO WAY!"]
 
     paths = []
+    dict = {}
     for s in start:
         path = bfs(s)
-        if path == ["NO WAY!"]:
+        if not path == ['NO WAY!']:
+            number_of_options = 0
+            for key, value in path.items():
+                number_of_options = len(value)
+                if number_of_options <= 3:
+                    l = 3 - number_of_options
+                    for i in range(l):
+                        i = number_of_options+i+1
+                        string = 'Opzione'+f'{i}'
+                        if string not in path:
+                            path[key][string] = {}
+                            path[key][string] = "NO WAY!"
             paths.append(path)
         else:
-            paths.extend(path)
+            paths.append("NO WAY!")
     return paths
+
+
 
 maze = [[1, 0, 1, 1, 1, 1],
         [4, 1, 1, 0, 1, 1],
         [1, 1, 1, 0, 1, 1],
         [0, 0, 1, 1, 4, 1],
-        [1, 1, 1, 1, 0, 1],
+        [1, 1, 1, 1, 1, 0],
         [1, 1, 1, 1, 1, 1]]
 
 start = [(0, 0), (2, 2), (4, 4)]
