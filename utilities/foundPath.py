2870abdd72ed5c7b4cc99e1941b5a9ed170b94e3
Federico Fiorio <federico.fiorio@outlook.it>
2023-02-09 16:22:59 +0100

    Finito Il codice di ricerca con output corretti

--- utilities/foundPath.py
+++ utilities/foundPath.py
@@ -1,12 +1,10 @@
-# TODO aggiungere i commenti alle funzioni più piccole all'interno del codice seguente
-# TODO vedere se aggiungere try except
-
-
 from heapq import heappop, heappush
 import numpy as np
 import json
 import os
 import time
+from typing import List, Tuple
+from heapq import heappop, heappush
 
 
 class FoundPath:
@@ -17,13 +15,176 @@ class FoundPath:
     __mydict = {}
     pass
 
-    def __init__(self, maze, start, goal):
+    def __init__(self, maze: List[List[int]], start: List[Tuple[int, int]], goal: Tuple[int, int], breadcrumps):
         self.__path_returned_json = None
         self.__maze = maze
         self.__start = start
         self.__goal = goal
+        self.__breadcrumps = breadcrumps
 
     pass
+
+
+    """
+    
+    Questo codice implementa l'algoritmo di Dijkstra per trovare il percorso più breve in un labirinto.
+    Il labirinto è rappresentato come una matrice dove i valori 0 rappresentano i muri e i valori diversi da 0 rappresentano i percorsi. 
+    L'algoritmo utilizza una coda di priorità per tenere traccia dei nodi da visitare, dove la priorità di ogni nodo è la somma del costo attuale
+    e della distanza euristica dalla posizione corrente al goal. Il codice utilizza la funzione "heappush" per inserire i nodi nella coda e "heappop"
+    per prendere il nodo con priorità minima.
+
+    La funzione "bfs" esegue l'algoritmo di Dijkstra, inizializzando la coda di priorità con la posizione di partenza,
+    la lista dei nodi visitati e la lista dei percorsi trovati. Il codice continua a ciclare finché non sono stati trovati 3 percorsi
+    o la coda è vuota. Se la posizione corrente è il goal, il percorso viene aggiunto alla lista dei percorsi e al dizionario.
+    Se il nodo corrente è già stato visitato, il codice passa al prossimo.
+    Altrimenti, espande il nodo corrente in tutte le direzioni possibili (Est, Ovest, Nord, Sud)
+    e inserisce i nuovi nodi nella coda.
+    
+    """
+    def deikstra(self):
+        maze = self.__maze
+        start = self.__start
+        goal = self.__goal
+        # Definizione delle variabili ROW e COL che
+        # rappresentano rispettivamente il numero di righe e colonne del labirinto.
+        ROW, COL = len(maze), len(maze[0])
+        # Definizione della lista "directions" che rappresenta le direzioni
+        # possibili da esplorare nel labirinto (Est, Ovest, Nord, Sud).
+        directions = ["E", "W", "N", "S"]
+
+        # Definizione della funzione "is_valid" che verifica se una posizione
+        # è valida (all'interno dei limiti del labirinto e non bloccata da un muro).
+        def is_valid(x, y):
+            return 0 <= x < ROW and 0 <= y < COL and maze[x][y] != 0
+
+        # Definizione della funzione "heuristic" che calcola la distanza
+        # euristica tra la posizione corrente e la posizione di destinazione
+        def heuristic(current, goal):
+            x1, y1 = current
+            x2, y2 = goal
+            return abs(x1 - x2) + abs(y1 - y2)
+
+        def bfs(start):
+            # Inizializza la coda con la priorità, che in questo caso è la somma del costo attuale e
+            # dell'euristica dalla posizione corrente al goal
+            pr_queue = []
+            heappush(pr_queue, (0 + heuristic(start, goal), 0, "", [], start))
+
+            # Inizializza la lista dei nodi visitati
+            visited = set()
+
+            # Inizializza la lista dei percorsi trovati
+            paths = []
+
+            # Inizializza il dizionario che conterrà tutti i percorsi
+            dictPath = {}
+
+            # Inizializza la lista che conterrà i punti analizzati
+            visitedPath = []
+
+            # Continua a ciclare finché non sono stati trovati 3 percorsi o la coda è vuota
+            while len(paths) < 3 and pr_queue:
+                # Prendi il nodo con priorità minima dalla coda
+                _, cost, path, path_cords, current = heappop(pr_queue)
+
+                # Se il nodo corrente è il goal, aggiungi il percorso alla lista dei percorsi e al dizionario
+                if current == goal:
+                    paths.append((start, goal, path, cost))
+                    nomeVar = 'PercorsoDa' + f'{start}'
+                    string = 'Opzione' + f'{len(paths)}'
+                    if nomeVar not in dictPath:
+                        dictPath[nomeVar] = {}
+                    if string not in dictPath:
+                        dictPath[nomeVar][string] = {}
+                    dictPath[nomeVar][string]["start"] = start
+                    dictPath[nomeVar][string]["goal"] = goal
+                    dictPath[nomeVar][string]["path"] = path
+                    dictPath[nomeVar][string]["numberOfMoviment"] = len(path)
+                    dictPath[nomeVar][string]["cost"] = cost
+                    dictPath[nomeVar][string]["movimentPath"] = path_cords
+                    #Aggiunto solo per verificare la uguale correttezza del path
+                    #dictPath[nomeVar][string]["LenMovimentPath"] = len(path_cords)
+                    #Scommentare per aggiungere il visited path al Json
+                    #dictPath[nomeVar][string]["visited"] = visitedPath
+                # Se il nodo è già stato visitato, salta al prossimo
+                if current in visited:
+                    continue
+
+                # Aggiungi il nodo corrente alla lista dei nodi visitati
+                visited.add(current)
+                visitedPath.append(current)
+
+                # Espandi il nodo corrente in tutte le direzioni possibili
+                x, y = current
+                for direction in directions:
+                    if direction == "E":
+                        if is_valid(x, y + 1):
+                            heappush(pr_queue,
+                                     (cost + maze[x][y + 1] + heuristic((x, y + 1), goal), cost + maze[x][y + 1],
+                                      path + direction, path_cords + [(x, y + 1)], (x, y + 1)))
+                    if direction == "W":
+                        if is_valid(x, y - 1):
+                            heappush(pr_queue,
+                                     (cost + maze[x][y - 1] + heuristic((x, y - 1), goal), cost + maze[x][y - 1],
+                                      path + direction, path_cords + [(x, y - 1)], (x, y - 1)))
+                    if direction == "N":
+                        if is_valid(x - 1, y):
+                            heappush(pr_queue,
+                                     (cost + maze[x - 1][y] + heuristic((x - 1, y), goal), cost + maze[x - 1][y],
+                                      path + direction, path_cords + [(x - 1, y)], (x - 1, y)))
+                    if direction == "S":
+                        if is_valid(x + 1, y):
+                            heappush(pr_queue,
+                                     (cost + maze[x + 1][y] + heuristic((x + 1, y), goal), cost + maze[x + 1][y],
+                                      path + direction, path_cords + [(x + 1, y)], (x + 1, y)))
+
+            # Viene ritornato o il percorso o No WAY
+            return dictPath or ["NO WAY!"]
+
+        # La lista "paths" conterrà i risultati della ricerca per ogni punto di partenza
+        paths = []
+        #Se si vuole aggiunge breadcrumps al json
+        #data = {"breadcrumps": list(self.__breadcrumps)}
+        #paths.append(data)
+        # Per ogni punto di partenza nella lista "start"
+        for s in start:
+            # Esegui la ricerca BFS per questo punto di partenza
+            path = bfs(s)
+
+            # Se la ricerca non restituisce "NO WAY!",
+            if not path == ['NO WAY!']:
+                # conta il numero di opzioni trovate
+                number_of_options = 0
+                for key, value in path.items():
+                    number_of_options = len(value)
+
+                    # Se il numero di opzioni è inferiore a 3,
+                    if number_of_options <= 3:
+                        # calcola la differenza per raggiungere 3
+                        l = 3 - number_of_options
+
+                        # e per ogni opzione mancante,
+                        for i in range(l):
+                            i = number_of_options + i + 1
+
+                            # inserisci una voce "NO WAY!" nella lista
+                            string = 'Opzione' + f'{i}'
+                            if string not in path:
+                                path[key][string] = {}
+                                path[key][string] = "NO WAY!"
+
+                # Aggiungi il risultato della ricerca a "paths"
+                paths.append(path)
+
+            # Se la ricerca restituisce "NO WAY!",
+            else:
+                # Aggiungi "NO WAY!" a "path"
+                path.append("NO WAY!")
+
+        # Restituisci il risultato della ricerca
+        self.__path_return = paths
+        return paths
+
     """
     Input Example:
     [
@@ -65,6 +226,7 @@ class FoundPath:
      Ho deciso di usufruire della convenzione che i muri assumessero il valore 0 e il percorso percorribile il valore 1 
      o superiore in caso di caselle di colr grigio in base alla gradazione si pone il valore da modificare. 
     """
+
 
     def maze2graph(self):
         height = len(self.__maze)
@@ -146,6 +308,8 @@ class FoundPath:
         And Cost for moviment
     """
 
+
+
     #Ultima opzione di codice
     def find_multi_path_astar_return_visited(self, breadcrumps, num_paths=3):
         goal = self.__goal
@@ -211,12 +375,11 @@ class FoundPath:
     def write_json_file(self, file_path, file_name):
         #Si richiama al controllo dell'esistenza del file
         self.ensure_path_exists(file_path)
-        self.ensure_path_exists('img_input')
         # richiamata la funzione di unicità del file con aggiunta al nome del time stamp
         name = self.unique_file_name(file_name)
         # viene scritto il file nel percorso di destinazione
         with open(file_path+name, "w") as json_file:
-            json.dump(self.__mydict, json_file, indent=4)
+            json.dump(self.__path_return, json_file, indent=4)
 
         # Funzione che permette di scrivere un file json
     def write_json_file_from_dumps(self, data, file_path, file_name):
